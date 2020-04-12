a1aa8df42d279e3438f177e65b3544a6dea74dfc
AakashNandwani <63542869+AakashNandwani@users.noreply.github.com>
2020-04-12 17:26:26 +0530

    Add files via upload
    Splitting and sorting all words of a file in ascending order.

---
+++ ass8_4.py
@@ -0,0 +1,14 @@
+#Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. When the program completes, sort and print the resulting words in alphabetical order.
+#You can download the sample data at http://www.py4e.com/code3/romeo.txt
+inp = input('Enter file : ')
+handle = open(inp)
+ans = list()
+for i in handle:
+    i = i.rstrip()
+    x = i.split()
+    for z in x:
+        if z not in ans:
+            ans.append(z)
+ans.sort()
+print(ans)            
+    
